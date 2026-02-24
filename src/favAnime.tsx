5cc171f61d9a95c7baa1cea1d883f50a2937c5d3
orde2201 <krisna2201yoga@gmail.com>
2026-02-24 09:42:10 +0700

    skahd

--- src/favAnime.tsx
+++ src/favAnime.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { useRef } from 'react'
+//import { useState } from 'react'
+//import { useRef } from 'react'
 import './App.css'
 
 // Importing images from assets
 
 
 export default function FavoritAnime() {
-  const [animeList, setAnimeList] = useState([
+  const animeList = [
     { title: 'Naruto', genre: 'Action, Adventure', rating: 8.3 },
     { title: 'One Piece', genre: 'Action, Adventure', rating: 8.7 },
     { title: 'Attack on Titan', genre: 'Action, Drama', rating: 9.0 },
     { title: 'My Hero Academia', genre: 'Action, Superhero', rating: 8.0 },
     { title: 'Demon Slayer', genre: 'Action, Fantasy', rating: 8.5 },
-  ])
+  ]
 
   return (
     <div className="anime-list">
--- src/favMusic.tsx
+++ src/favMusic.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { useRef } from 'react'
+//import { useState } from 'react'
+//import { useRef } from 'react'
 import './App.css'
 
 // Importing images from assets
 
 
 export function FavoritMusic() {
-  const [musicList, setMusicList] = useState([
+  const musicList = [
     { title: 'Naruto', genre: 'Action, Adventure', rating: 8.3 },
     { title: 'One Piece', genre: 'Action, Adventure', rating: 8.7 },
     { title: 'Attack on Titan', genre: 'Action, Drama', rating: 9.0 },
     { title: 'My Hero Academia', genre: 'Action, Superhero', rating: 8.0 },
     { title: 'Demon Slayer', genre: 'Action, Fantasy', rating: 8.5 },
-  ])
+  ]
 
   return (
     <div className="music-list">
